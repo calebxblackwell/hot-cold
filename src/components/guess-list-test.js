d7c4c9630d7bc902e2e2fec8e96e7b6cf5ed0990
Caleb Blackwell <calebxblackwell@gmail.com>
2018-03-28 08:44:42 -0700

    updates

--- src/components/guess-list-test.js
+++ src/components/guess-list-test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import GuessList from './guess-list';
+
 
 describe('<GuessList />', () => {
   it('Renders without crashing', () => {
     shallow(<GuessList guesses={[]} />);
   });
-
   it('Renders a list of guesses', () => {
     const values = [10, 24, 52];
     const wrapper = shallow(<GuessList guesses={values} />);
--- src/components/status-section-test.js
+++ src/components/status-section-test.js
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import StatusSection from './status-section';
 
 describe('<StatusSection />', () => {
